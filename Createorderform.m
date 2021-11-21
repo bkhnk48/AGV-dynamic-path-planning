11e74c5b9541869d7b6eac26466e63e57ab91e95
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-11-21 11:32:41 +0700

    Create Createorderform.m
    Robot-simulation-in-a-warehouse-environment

---
+++ Createorderform.m
@@ -0,0 +1,18 @@
+function [point,goal] = Createorderform()
+%Generate orders
+point=[];
+global storagepoint;
+global target;
+N=size(storagepoint,1);
+n=size(target,1);
+nodenumber=unidrnd(3);          %Number of mission points generated
+
+task=randperm(N,nodenumber);
+for i=1:nodenumber
+    point(i,:)=storagepoint(task(i),:);
+end
+
+goalind=unidrnd(n);
+goal=target(goalind,1:2);
+
+end
