4bcd507c29779e4b77eb37083826b073337574bd
sedic <sediclab@outlook.com>
2021-11-23 16:32:56 +0700

    fix the message https://github.com/bkhnk48/AGV-dynamic-path-planning/issues/1#issue-1060769409

--- Createorderform.m
+++ Createorderform.m
@@ -6,6 +6,7 @@ global target;
 N=size(storagepoint,1);
 n=size(target,1);
 nodenumber=unidrnd(3);          %Number of mission points generated
+%list of intermediate working points of AGV
 printf("Node number = %d\n", nodenumber);
 
 task=randperm(N,nodenumber);
@@ -13,7 +14,8 @@ for i=1:nodenumber
     point(i,:)=storagepoint(task(i),:);
 end
 
-goalind=unidrnd(n);
+goalind=unidrnd(n); %randomize the final point of the AGV in it's journey
+%n is the number of all final points in the environment
 goal=target(goalind,1:2);
 
 end
