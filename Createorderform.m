451da2e0f42cabc9ccc6389af55e6bebdfdbf2e4
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-11-25 14:22:06 +0700

    Print out the imtermediate dest points as well as
    goal ones

--- Createorderform.m
+++ Createorderform.m
@@ -12,10 +12,11 @@ printf("Node number = %d\n", nodenumber);
 task=randperm(N,nodenumber);
 for i=1:nodenumber
     point(i,:)=storagepoint(task(i),:);
+    printf("Intermediate dest: [%.2f %.2f]\n", point(i, 1), point(i, 2));
 end
 
 goalind=unidrnd(n); %randomize the final point of the AGV in it's journey
 %n is the number of all final points in the environment
 goal=target(goalind,1:2);
-
+printf("Goal: [%.2f %.2f]\n", goal(1, 1), goal(1, 2));
 end
