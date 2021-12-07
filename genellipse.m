c0a163cfedc969e4bfcabc620b6ccfafb26672b8
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-12-07 07:16:22 +0700

    a func for generate ellipse trajectory

---
+++ genellipse.m
@@ -0,0 +1,26 @@
+function  [xAgent, yAgent] = genellipse(m)
+  xc=50; %xCenter
+  %xc = input('Enter the coord of xCenter: ');
+  yc=50; %yCenter
+  %yc = input('Enter the coord of yCenter: ');
+  a=25; %xRad
+  b=100; %yRad
+  %m = 1000;
+  x = zeros(m,1);
+  y = zeros(m,1);
+  theta = linspace(0,2*pi,m);
+  for k = 1:m
+        x(k) = a * cos(theta(k));
+        y(k) = b * sin(theta(k));
+  end
+  %alpha = input('Enter the rotation angle: ');
+  alpha = 17; %unit: radian
+  R  = [cos(alpha) -sin(alpha); ...
+        sin(alpha)  cos(alpha)];
+  rCoords = R*[x' ; y'];   
+  xr = rCoords(1,:)';      
+  yr = rCoords(2,:)';      
+  
+  xAgent = xr + xc;
+  yAgent = yr + yc;
+ end
